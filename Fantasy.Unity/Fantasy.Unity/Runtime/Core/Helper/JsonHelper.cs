64990f5582f919c2f4b02be4cada5a068b273c3f
bazzisl <564697081@qq.com>
2025-04-02 01:29:43 +0800

    增加json的压缩等方法

--- Fantasy.Unity/Fantasy.Unity/Runtime/Core/Helper/JsonHelper.cs
+++ Fantasy.Unity/Fantasy.Unity/Runtime/Core/Helper/JsonHelper.cs
@@ -1,5 +1,9 @@
 using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 #pragma warning disable CS8603
 
 namespace Fantasy.Helper
@@ -52,6 +56,138 @@ namespace Fantasy.Helper
         public static T Clone<T>(T t)
         {
             return t.ToJson().Deserialize<T>();
+        }
+
+        /// <summary>
+        /// 压缩JSON字符串，移除不必要的空白字符。
+        /// </summary>
+        /// <param name="json">要压缩的JSON字符串。</param>
+        /// <returns>压缩后的JSON字符串。</returns>
+        public static string CompressJson(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+            {
+                return json;
+            }
+
+            try
+            {
+                var obj = JToken.Parse(json);
+                return obj.ToString(Newtonsoft.Json.Formatting.None);
+            }
+            catch
+            {
+                return json; // 如果解析失败，返回原始字符串
+            }
+        }
+
+        /// <summary>
+        /// 美化JSON字符串，添加适当的缩进和换行。
+        /// </summary>
+        /// <param name="json">要美化的JSON字符串。</param>
+        /// <returns>美化后的JSON字符串。</returns>
+        public static string PrettyJson(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+            {
+                return json;
+            }
+
+            try
+            {
+                var obj = JToken.Parse(json);
+                return obj.ToString(Newtonsoft.Json.Formatting.Indented);
+            }
+            catch
+            {
+                return json; // 如果解析失败，返回原始字符串
+            }
+        }
+
+        /// <summary>
+        /// 将JSON字符串转换为二进制数据。
+        /// </summary>
+        /// <param name="json">要转换的JSON字符串。</param>
+        /// <returns>转换后的二进制数据。</returns>
+        public static byte[] JsonToBinary(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+            {
+                return Array.Empty<byte>();
+            }
+
+            // 先压缩JSON字符串
+            string compressedJson = CompressJson(json);
+            byte[] jsonBytes = Encoding.UTF8.GetBytes(compressedJson);
+
+            // 使用GZip进一步压缩
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (GZipStream gzip = new GZipStream(ms, CompressionMode.Compress, true))
+                {
+                    gzip.Write(jsonBytes, 0, jsonBytes.Length);
+                }
+                return ms.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// 将二进制数据转换回JSON字符串。
+        /// </summary>
+        /// <param name="binaryData">二进制数据。</param>
+        /// <returns>转换后的JSON字符串。</returns>
+        public static string BinaryToJson(byte[] binaryData)
+        {
+            if (binaryData == null || binaryData.Length == 0)
+            {
+                return string.Empty;
+            }
+
+            using (MemoryStream ms = new MemoryStream(binaryData))
+            {
+                using (MemoryStream decompressedMs = new MemoryStream())
+                {
+                    using (GZipStream gzip = new GZipStream(ms, CompressionMode.Decompress))
+                    {
+                        gzip.CopyTo(decompressedMs);
+                    }
+                    return Encoding.UTF8.GetString(decompressedMs.ToArray());
+                }
+            }
+        }
+
+        /// <summary>
+        /// 将对象直接序列化为二进制数据。
+        /// </summary>
+        /// <typeparam name="T">要序列化的对象类型。</typeparam>
+        /// <param name="obj">要序列化的对象。</param>
+        /// <returns>序列化后的二进制数据。</returns>
+        public static byte[] ToBinary<T>(T obj)
+        {
+            if (obj == null)
+            {
+                return Array.Empty<byte>();
+            }
+            
+            string json = ToJson(obj);
+            return JsonToBinary(json);
+        }
+
+        /// <summary>
+        /// 从二进制数据反序列化为指定类型的对象。
+        /// </summary>
+        /// <typeparam name="T">目标对象的类型。</typeparam>
+        /// <param name="binaryData">二进制数据。</param>
+        /// <returns>反序列化后的对象。</returns>
+        public static T FromBinary<T>(byte[] binaryData)
+        {
+            if (binaryData == null || binaryData.Length == 0)
+            {
+                return default;
+            }
+            
+            string json = BinaryToJson(binaryData);
+            return Deserialize<T>(json);
         }
     }
 }
